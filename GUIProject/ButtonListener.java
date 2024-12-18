91be807e668275025d6cd0c971ff1a200443fe60
Phan Sỹ Hùng <135130980+KumaMichio@users.noreply.github.com>
2024-12-18 17:27:20 +0700

    Update ButtonListener.java

--- GUIProject/ButtonListener.java
+++ GUIProject/ButtonListener.java
@@ -1,22 +1,30 @@
 package hust.soict.hedspi.swing;
 
 import java.awt.event.ActionEvent;
+
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JPanel;
+
 
 public class ButtonListener implements ActionListener {
+	private NumberGrid numberGrid;
+	
+    public ButtonListener(NumberGrid numberGrid) {
+        this.numberGrid = numberGrid;
+    }
+    
 	@Override
     public void actionPerformed(ActionEvent e) {
         String button = e.getActionCommand();
         if (button.charAt(0) >= '0' && button.charAt(0) <= '9') {
-            tfDisplay.setText(tfDisplay.getText() + button);
+			numberGrid.getTfDisplay().setText(numberGrid.getTfDisplay().getText() + button);
         } else if (button.equals("DEL")) {
-            
-        } else {
-            // handles the "C" case
+            String text = numberGrid.getTfDisplay().getText();
+            if(!text.isEmpty()) {
+            	numberGrid.getTfDisplay().setText(text.substring(0, text.length() - 1));
+            }
+        } else if(button.equals("C")) {
+            numberGrid.getTfDisplay().setText("");
         }
     }
-    
 }
